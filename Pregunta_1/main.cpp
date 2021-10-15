33b63e987161e0efe30dc0047ef3b2e34ee7b48e
mmogaP <mmoraga.m3@gmail.com>
2021-10-14 23:21:40 -0300

    Coordenadas con numeros

--- Pregunta_1/main.cpp
+++ Pregunta_1/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char** argv){
     Tablero* ob1;
-    int x,y;
+    int x,y, opcion, n;
 
     cout<<"Digite la posicion inicial del objeto: "<<endl;
     cout<<"Posición X: "; cin>>x;
@@ -19,6 +19,26 @@ int main(int argc, char** argv){
         cout<<"3. Mover Este"<<endl;
         cout<<"4. Mover Oeste"<<endl;
         cout<<"5. Salir"<<endl;
-    }
+        cout<<"Digite una opción: "<<endl;
+        cin>>opcion;
+
+        if(opcion >=1 && opcion <= 4){
+            cout<<"Digite cuantas posiciones desea mover: ";
+            cin>>n;
+        }
+
+        switch(opcion){
+            case 1: ob1->moverNorte(n); break;
+            case 2: ob1->moverSur(n); break;
+            case 3: ob1->moverEste(n); break;
+            case 4: ob1->moverOeste(n); break;
+            case 5: break;
+            default: cout<<"\nSe equivoco de opcion de menu"<<endl;
+        }
+
+        cout<<"Posicion actual (X,Y): ("<<ob1->getX()<<","<<ob1->getY()<<")"<<endl;
+
+    }while(opcion != 5);
+
     return 0;
 }
